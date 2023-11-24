287b2d7b0970ef1490112ae3712d4921a8d31a37
niha-2 <gen.3polym.hn@gmail.com>
2023-11-24 20:10:35 +0900

    [fix] letter_openerの設定を追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,18 +41,18 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.perform_caching = false
+  # config.action_mailer.perform_caching = false
 
 
-  config.action_mailer.default_url_options = { host: localhost, port: 3000 }
+  # config.action_mailer.default_url_options = { host: localhost, port: 3000 }
   # `:letter_opener`を指定する
-  config.action_mailer.delivery_method = :letter_opener
-  # ログにエラーを表示するために`true`を設定
-  config.action_mailer.raise_delivery_errors = true
-
-
   # config.action_mailer.delivery_method = :letter_opener
-  # config.action_mailer.perform_deliveries = true
+  # ログにエラーを表示するために`true`を設定
+  # config.action_mailer.raise_delivery_errors = true
+
+
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
   # # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # config.action_mailer.browser = 'firefox'
 
