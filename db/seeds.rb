a4cf579d3b0b442479ca3b30d83be9fa25ec3f0b
niha-2 <gen.3polym.hn@gmail.com>
2023-12-06 22:55:23 +0900

    [fix] 本番環境用のseedファイルを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,8 @@
 product1 = Product.new(
   name: 'Aぇ! group ステッカー',
   description: 'Aぇ! groupのステッカーです。',
-  price: 4200
+  price: 4200,
+  deleted: false
 )
 product1.image.attach(io: File.open(Rails.root.join('app/assets/images/Agroup_steaker.jfif')),
                       filename: 'Agroup_steaker.jfif')
@@ -20,7 +21,8 @@ product1.save!
 product2 = Product.new(
   name: 'なにわ男子 うちわ',
   description: 'なにわ男子のうちわです。',
-  price: 800
+  price: 800,
+  deleted: false
 )
 product2.image.attach(io: File.open(Rails.root.join('app/assets/images/no_image.png')),
                       filename: 'no_image.png')
@@ -29,7 +31,8 @@ product2.save!
 product3 = Product.new(
   name: 'なにわ男子 クリアファイル',
   description: 'なにわ男子のクリアファイルです。',
-  price: 500
+  price: 500,
+  deleted: false
 )
 product3.image.attach(io: File.open(Rails.root.join('app/assets/images/naniwa_clearfilefolder.jpg')),
                       filename: 'naniwa_clearfilefolder.jpg')
@@ -38,7 +41,8 @@ product3.save!
 product4 = Product.new(
   name: 'SnowMan ポスター',
   description: 'SnowManのポスターです。',
-  price: 1000
+  price: 1000,
+  deleted: false
 )
 product4.image.attach(io: File.open(Rails.root.join('app/assets/images/no_image.png')),
                       filename: 'no_image.png')
@@ -47,7 +51,8 @@ product4.save!
 product5 = Product.new(
   name: 'SixTONES ペンライト',
   description: 'SixTONESのペンライトです。',
-  price: 1000
+  price: 1000,
+  deleted: false
 )
 product5.image.attach(io: File.open(Rails.root.join('app/assets/images/sixtones_light.jpg')),
                       filename: 'sixtones_light.jpg')
