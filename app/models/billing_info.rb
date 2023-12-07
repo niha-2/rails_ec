67849c4855e250bac9595b3548d2ee9557115613
niha-2 <gen.3polym.hn@gmail.com>
2023-12-07 22:44:24 +0900

    [fix] レビュー対応：not null制約のバリデーションの記載をまとめる

--- app/models/billing_info.rb
+++ app/models/billing_info.rb
@@ -2,16 +2,18 @@
 
 class BillingInfo < ApplicationRecord
   has_many :purchase_details, dependent: :destroy
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :user_name, presence: true
-  validates :address, presence: true
-  validates :country, presence: true
-  validates :state, presence: true
+  with_options presence: true do
+    validates :first_name
+    validates :last_name
+    validates :user_name
+    validates :address
+    validates :country
+    validates :state
+    validates :payment_method
+    validates :name_on_card
+    validates :credit_card_expiration
+  end
   validates :zip, presence: true, length: { is: 5 }
-  validates :payment_method, presence: true
-  validates :name_on_card, presence: true
   validates :credit_card_number, presence: true, length: { is: 16 }
-  validates :credit_card_expiration, presence: true
   validates :credit_card_cvv, presence: true, length: { is: 3 }
 end
